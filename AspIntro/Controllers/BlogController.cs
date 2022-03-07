65c178170b9045e7cf99df3c445aa2b60ade493a
[umid shamdinli] <[umidash@code.edu.az]>
2022-03-07 21:56:29 +0400

    Front

---
+++ AspIntro/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspIntro.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+    }
+}
